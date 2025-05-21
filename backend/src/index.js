1b271acde0ebac2c9c1273f6d69f40ca4385a7aa
namdng09 <namduong090104@gmail.com>
2025-05-21 22:49:00 +0700

    build: create backend folder using express

---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routers/auth.route.js";
+import messageRoutes from "./routers/message.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/message", messageRoutes);
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}/`);
+//   connectDB();
+// });
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Database is connected successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
