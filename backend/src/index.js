e8c4b0221decd12a9c5e01f12bf277ee3797b748
namdng09 <namduong090104@gmail.com>
2025-05-22 23:23:16 +0700

    feat: fix cors between backend and frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routers/auth.route.js";
@@ -15,6 +16,12 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/message", messageRoutes);
