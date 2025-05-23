ef47f3391c1415575c862d0f19c49257356147f8
namdng09 <namduong090104@gmail.com>
2025-05-23 22:19:53 +0700

    feat: add block to input email and password

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { MessageSquare, User } from "lucide-react";
+import { Eye, EyeOff, Mail, MessageSquare, User } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -45,8 +45,8 @@ const SignUpPage = () => {
                 <span className="label-text font-medium">Full Name</span>
               </label>
               <div className="relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <User className="size-5 text-base-content/60" />
+                <div className="absolute inset-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="z-10 size-5 text-base-content/60" />
                 </div>
                 <input
                   type="text"
@@ -57,6 +57,55 @@ const SignUpPage = () => {
                     setFormData({ ...formData, fullName: e.target.value })
                   }
                 />
+              </div>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="z-10 size-5 text-base-content/60" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="johndoe@gmail.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text font-medium">Password</span>
+                </label>
+                <div className="relative">
+                  <div className="absolute inset-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <Lock className="z-10 size-5 text-base-content/60" />
+                  </div>
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className={`input input-bordered w-full pl-10`}
+                    placeholder="••••••••"
+                    value={formData.password}
+                    onChange={(e) =>
+                      setFormData({ ...formData, password: e.target.value })
+                    }
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? (
+                      <EyeOff className="size-5 text-base-content/40" />
+                    ) : (
+                      <Eye className="size-5 text-base-content/40" />
+                    )}
+                  </button>
+                </div>
               </div>
             </div>
           </form>
